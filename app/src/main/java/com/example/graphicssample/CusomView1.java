f3db7de4db7ff61355b9087c50987e4db763403d
unknown <s19B710@shohoku.ac.jp>
2020-10-02 11:35:47 +0900

    カスタムびゅーだお

---
+++ app/src/main/java/com/example/graphicssample/CusomView1.java
@@ -0,0 +1,21 @@
+package com.example.graphicssample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CusomView1 extends View {
+    public CusomView1(Context context) {
+        super(context);
+    }
+
+    public CusomView1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CusomView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/example/graphicssample/CusomView2.java
@@ -0,0 +1,21 @@
+package com.example.graphicssample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CusomView2 extends View {
+    public CusomView2(Context context) {
+        super(context);
+    }
+
+    public CusomView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CusomView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
